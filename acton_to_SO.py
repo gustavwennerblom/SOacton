e2d75a051bb1f9eab124d019e67e3c9e0605884d
gustavwennerblom <gustav.wennerblom@gmail.com>
2017-03-31 23:27:04 +0200

    working auth/refresh flow

--- acton_to_SO.py
+++ acton_to_SO.py
@@ -1,35 +1,54 @@
 import logging
 import json
+from requests.exceptions import HTTPError
 from actonsession import ActonSession
 
+
 if __name__ == '__main__':
+    # Create logging instance
+    FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(filename="SOacton.log", format=FORMAT, level=logging.INFO)
+
+    # Temporary sandbox option exposure
     usr_in = input("Menu \n"
                    "(1) Work with sandbox\n"
                    "(2) Work with live account\n"
                    ">>")
 
+    # Read credentials (only used if working with production environment"
     with open("CREDENTIALS.json") as f:
         credentials = json.loads(f.readline())
         CLIENT_ID = credentials['client-id']
         CLIENT_SECRET = credentials['client-secret']
 
-    if usr_in == 2:
+    if usr_in == "2":
         username = input("Please enter Act-On username")
         password = input("Please enter Act-On password")
-    elif usr_in == 1:
-        username = 'crmcase@business-sweden.se'
+    elif usr_in == "1":
+        username = credentials["sandbox-UID"]
         password = 'welcome'
     else:
         print("No valid selection made, passing on to sandbox")
-        username = 'crmcase@business-sweden.se'
+        username = credentials["sandbox-UID"]
         password = 'welcome'
 
+    # Create session manager instance
     session = ActonSession()
 
-    # noinspection PyBroadException
+    # Testing authorization
     try:
         access_key, refresh_key = session.authenticate(username, password, CLIENT_ID, CLIENT_SECRET)
-    except:     # TO BE REMOVED AFTER BUG FIXING
-        "Fatal error upon attempt to authenticate. Dropping to shell"
-        import code
-        code.interact(local=locals())
+        logging.info("Successfully authenticated.\n Primary key: {0}, refresh key: {1}".format(access_key, refresh_key))
+    except HTTPError as e:
+        print("Fatal error upon attempt to authenticate. Dropping to shell")
+        print(repr(e))
+
+    while True:
+        input("Press enter to attempt session refresh")
+
+        # Testing refresh
+        try:
+            access_key, refresh_key = session.renew_token(CLIENT_ID, CLIENT_SECRET)
+            logging.info("Successfully refreshed.\n Primary key: {0}, refresh key: {1}".format(access_key, refresh_key))
+        except HTTPError as e:     # TO BE REFINED
+            print(repr(e))
--- actonsession.py
+++ actonsession.py
@@ -1,14 +1,15 @@
+import html
 import logging
 import requests
 from requests.compat import urljoin
 
 
-def wrap(key):
-    wrappedkey = '<'+key+'>'
-    return wrappedkey
-
-
 class ActonSession:
+
+    @staticmethod
+    def wrap(key):
+        wrappedkey = html.escape("<") + key + html.escape(">")
+        return wrappedkey
 
     def authenticate(self, username, password, client_id, client_secret):
         path = '/token'
@@ -35,7 +36,7 @@ class ActonSession:
         else:
             r.raise_for_status()
 
-    def renew_token(self):
+    def renew_token(self, client_id, client_secret):
         path = '/token'
         url = urljoin(self.HOST, path)
         headers = {'Cache-Control': 'no-cache',
@@ -43,17 +44,20 @@ class ActonSession:
                    }
 
         payload = {'grant_type': 'refresh_token',
-                   'refresh_token': wrap(self.REFRESH_TOKEN),
-                   'client_id': wrap(self.CLIENT_ID),
-                   'client_secret': wrap(self.CLIENT_SECRET)
+                   'refresh_token': self.REFRESH_TOKEN,
+                   'client_id': client_id,
+                   'client_secret': client_secret
                    }
 
         r = requests.post(url, headers=headers, params=payload)
+        # print("Request: {}".format(r.text))
 
         if not r.raise_for_status():
             response = r.json()
+            # print("Response: {}".format(response))
             self.ACCESS_TOKEN = response['access_token']
             self.REFRESH_TOKEN = response['refresh_token']
+            return self.ACCESS_TOKEN, self.REFRESH_TOKEN
         else:
             r.raise_for_status()
 
@@ -67,4 +71,3 @@ class ActonSession:
         self.REFRESH_TOKEN = ''
         self.CLIENT_ID = ''
         self.CLIENT_SECRET = ''
-
